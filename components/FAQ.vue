c77e0e0863b5413e738ea1480fe3897c256cc82d
Giulio Rossi <ciuffi9@mac.com>
2019-11-04 16:19:45 -0800

    Better styling for FAQ section

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -2,58 +2,126 @@
   <div>
     <p :class="`title is-4 ${darkmodeText}`">FAQ</p>
 
-    <div>
-      <button @click="handleAddition()">Add FAQ</button>
+    <div style="display: flex;">
+      <button
+        style="margin: auto 0;"
+        :class="`button is-primary ${isLight}`"
+        @click="handleAddition()"
+      >
+        Add a FAQ Question
+      </button>
     </div>
-    <div class="FAQs">
-      <div v-for="faq in listOfFaq" :key="faq.id">
-        <br />
-        <input id="checkbox" v-model="faq.data.selected" type="checkbox" />
-        Question:
-        {{ faq.data.question }}
-        <br />
-        Category:
-        {{ faq.data.category }}
-        <br />
-        Answer:
-        {{ faq.data.answer }}
-        <br />
-        Timestamp:
-        {{ new Date(faq.data.timestamp.toDate()) }}
-        <br />
-        Last edited by:
-        {{ faq.data.user }}
-        <br />
-        <button @click="handleEdit(faq)">
-          Edit
-        </button>
+    <div>
+      <div :class="`table-container`">
+        <table
+          :class="
+            `table is-fullwidth is-scrollable has-background-black ${darkmodeText}`
+          "
+        >
+          <thead>
+            <tr>
+              <th :class="darkmodeText">
+                Catagory
+              </th>
+              <th :class="darkmodeText">
+                Question
+              </th>
+              <th :class="darkmodeText">
+                Answer
+              </th>
+              <th :class="darkmodeText">
+                Edit
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="faq in listOfFaq" :key="faq.id">
+              <td>
+                {{ faq.data.category }}
+              </td>
+              <td>
+                {{ faq.data.question }}
+              </td>
+              <td>
+                {{ faq.data.answer }}
+              </td>
+              <td style="vertical-align: middle;">
+                <button
+                  :class="`button is-primary ${isLight}`"
+                  @click="handleEdit(faq)"
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
-    <b-modal class="has-text-white" :active.sync="editMode" :can-cancel="false">
-      <p :class="`title is-4 has-text-white`">Edit</p>
-      <br />
-      Selected?
-      <input id="checkbox" v-model="data.selected" type="checkbox" />
-      <br />
-      Question:
-      <input v-model="data.question" />
-      <br />
-      Catagory:
-      <select v-model="data.category">
-        <option disabled value="">Please select one</option>
-        <option>General</option>
-        <option>Teams</option>
-        <option>Logistics</option>
-      </select>
-      <br />
-      Answer:
-      <input v-model="data.answer" />
-      <br />
-      <button @click="handleSave()">
-        Save
-      </button>
-      <button @click="handleRemoval()">Delete</button>
-      <button @click="editMode = !editMode">Exit</button>
+    <b-modal
+      class="has-text-white has-modal-card"
+      :active.sync="showPanel"
+      :can-cancel="false"
+    >
+      <div class="modal-card">
+        <header class="modal-card-head has-background-dark">
+          <p class="modal-card-title has-text-light">
+            {{ editMode ? 'Editing a FAQ' : 'Add a FAQ' }}
+          </p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section :class="`modal-card-body has-background-dark`">
+          <div style="display: flex">
+            <p style="width: 15%;" class="has-text-light title is-6">
+              Catagory:
+            </p>
+            <div class="select is-small is-primary">
+              <select v-model="data.category">
+                <option disabled value="placeholder">Please select one</option>
+                <option>General</option>
+                <option>Teams</option>
+                <option>Logistics</option>
+              </select>
+            </div>
+          </div>
+          <div style="display: flex;">
+            <p style="width: 15%;" :class="`title is-6 has-text-light`">
+              Question:
+            </p>
+            <input
+              v-model="data.question"
+              class="input is-primary is-small is-narrow"
+            />
+          </div>
+          Answer:
+          <textarea
+            v-model="data.answer"
+            class="textarea is-success is-small"
+            placeholder="Answer"
+            rows="3"
+          ></textarea>
+          <br />
+          <button class="button is-small is-success" @click="handleSave()">
+            Save
+          </button>
+          <button
+            v-if="editMode"
+            class="button is-small is-danger"
+            @click="handleRemoval()"
+          >
+            Delete
+          </button>
+          <button
+            class="button is-warning is-small"
+            @click="
+              editMode = false
+              addMode = false
+            "
+          >
+            Exit
+          </button>
+        </section>
+      </div>
     </b-modal>
   </div>
 </template>
@@ -85,6 +153,7 @@ export default {
   data() {
     return {
       editMode: false,
+      addMode: false,
       currentFaq: {},
       data: {
         selected: true,
@@ -94,14 +163,19 @@ export default {
       }
     }
   },
+  computed: {
+    showPanel() {
+      return this.addMode || this.editMode
+    }
+  },
   mounted() {
     this.refreshData()
   },
   methods: {
     handleEdit(faq) {
-      this.editMode = !this.editMode
+      this.editMode = true
       this.currentFaq = faq
-      this.data.selected = faq.data.selected
+      this.data.selected = true
       this.data.question = faq.data.question
       this.data.category = faq.data.category
       this.data.answer = faq.data.answer
@@ -117,20 +191,21 @@ export default {
       } else {
         fireDb.add(this.website, 'Faq', this.data)
       }
-      this.editMode = !this.editMode
+      this.editMode = false
+      this.addMode = false
       this.refreshData()
     },
     handleAddition() {
       this.currentFaq = null
-      this.editMode = !this.editMode
+      this.addMode = true
       this.data.selected = false
       this.data.question = 'Sample question'
-      this.data.category = 'Sample category'
+      this.data.category = 'placeholder'
       this.data.answer = 'Sample answer'
       this.$emit('refreshData')
     },
     handleRemoval() {
-      this.editMode = !this.editMode
+      this.editMode = false
       if (this.currentFaq != null && this.currentFaq.id !== undefined) {
         fireDb.delete(this.website, 'Faq', this.currentFaq.id)
       }
@@ -142,9 +217,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-.FAQs {
-  height: 500px;
-  overflow-y: auto;
-}
-</style>
